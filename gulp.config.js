9c06722a45ed777d9a795b65e9c910577ccdada2
Evgeny Gurtovoy <Andlimer@gmail.com>
2020-09-29 14:54:48 +1000

    Делаю слайдер каталога

--- gulp.config.js
+++ gulp.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   SRC_PATH: 'src',
   DIST_PATH: 'dist',
-  STYLES_LIBS: ['node_modules/normalize.css/normalize.css'],
-  JS_LIBS: []
+  STYLES_LIBS: [
+    'node_modules/normalize.css/normalize.css'
+    // 'node_modules/slick-carousel/slick/slick.css'
+  ],
+  JS_LIBS: [
+    'node_modules/jquery/dist/jquery.min.js',
+    'node_modules/slick-carousel/slick/slick.min.js'
+  ]
 }
---
+++ src/js/works-slider.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('.works-slider').slick({
+    prevArrow: $('.slider-arrow__prev'),
+    nextArrow: $('.slider-arrow__next'),
+    dots: true,
+    adaptiveHeight: true
+  });
+});

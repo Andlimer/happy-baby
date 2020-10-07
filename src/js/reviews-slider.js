50e0d453a24ec47bc377592b69197d9066b9d754
Evgeny Gurtovoy <Andlimer@gmail.com>
2020-10-07 23:46:13 +1000

    Переделываю секцию hero

--- src/js/reviews-slider.js
+++ src/js/reviews-slider.js
@@ -2,9 +2,10 @@ $(document).ready(function() {
   $('.reviews-slider__slider').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToScroll: 1,
     prevArrow: $('.reviews-slider__prev'),
     nextArrow: $('.reviews-slider__next'),
+    appendDots: $('.reviews-slider__dots'),
     dots: true,
     variableWidth: true
   });
